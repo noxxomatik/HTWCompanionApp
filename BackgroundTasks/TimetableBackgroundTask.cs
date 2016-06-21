d11f0c0bc264b8d206c39b11db3a3545b9e0948b
noxxomatik <noxxomatik@live.de>
2016-06-21 23:18:48 +0200

    ADD: show notification if user has new grades
    checks for new grades every 15 minutes

--- BackgroundTasks/TimetableBackgroundTask.cs
+++ BackgroundTasks/TimetableBackgroundTask.cs
@@ -1,32 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Windows.ApplicationModel.Background;
 using Windows.Data.Xml.Dom;
-using Windows.UI.Notifications;
-using Windows.Web.Syndication;
-using System.IO;
-using System.Net;
-using System.Net.Http;
-using System.Runtime.Serialization;
-using System.Text.RegularExpressions;
-
-using Windows.Storage;
-using NotificationsExtensions.Tiles;
-using NotificationsExtensions.Toasts;
 using HTWAppObjects;
-using System.Globalization;
+using NotificationsExtensions.Toasts;
+using Windows.UI.Notifications;
 
-namespace BackgroundTasks {
+namespace BackgroundTasks
+{
     public sealed class TimetableBackgroundTask : IBackgroundTask {
         public async void Run(IBackgroundTaskInstance taskInstance) {
             // Get a deferral, to prevent the task from closing prematurely 
             // while asynchronous code is still running.
             BackgroundTaskDeferral deferral = taskInstance.GetDeferral();
 
+            // update live tile
             try {
                 // Load the item.
                 XmlDocument tileXml = await TimetableObject.GetNextLessonXml();
@@ -35,12 +23,39 @@ namespace BackgroundTasks {
                 // Update the live tile with the item.
                 TimetableObject.UpdateTile(tileXml);
             }
-            catch (Exception e) {
-                Debug.WriteLine("Missing timteable item. First load the timetable.");
+            catch () {
+                Debug.WriteLine("Missing timetable item. First load the timetable.");
+            }
+
+            // check for new grades
+            try {
+                GradesModel gm = GradesModel.getInstance();
+                SettingsModel sm = SettingsModel.getInstance();
+                int countNewGrades = await gm.getNewGradesCount(sm.SNummer, sm.RZLogin);
+                if (countNewGrades > 0) {
+                    ToastContent content = new ToastContent() {
+                        Visual = new ToastVisual() {
+                            TitleText = new ToastText() {
+                                Text = countNewGrades + " neue Noten verfügbar!"
+                            }
+                        },
+                        Audio = new ToastAudio() {
+                            Src = new Uri("ms-winsoundevent:Notification.IM")
+                        }
+                    };
+                    XmlDocument xmlContent = content.GetXml();
+                    var toast = new ToastNotification(xmlContent);
+                    ToastNotificationManager.CreateToastNotifier().Show(toast);
+                }
+            }
+            catch () {
+                Debug.WriteLine("Grades could not be retrieved.");
             }
 
             // Inform the system that the task is finished.
             deferral.Complete();
         }
+
+        
     }
 }
