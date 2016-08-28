f41584bef8bdf63d292b70715c6149c2ba1c3081
noxxomatik <noxxomatik@live.de>
2016-08-28 12:54:04 +0200

    ADD: template 10 project

---
+++ HTWDDAppUniversal/Views/DetailPage.xaml.cs
@@ -0,0 +1,15 @@
+using HTWDDAppUniversal.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace HTWDDAppUniversal.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage() {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
---
+++ HTWDDAppUniversal/Views/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+using System;
+using HTWDDAppUniversal.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace HTWDDAppUniversal.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage() {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
